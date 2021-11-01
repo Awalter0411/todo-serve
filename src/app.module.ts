40c5af3f30d6901720e33a24db9b7ed0f076a496
Twitzz <2351061127@qq.com>
2021-11-01 13:37:20 +0800

    使用配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { TodoModule } from './modules/todos/todos.module';
 import { CategoriesModule } from './modules/categories/categories.module';
-
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'lyxa1105',
-    database: 'todo',
-    autoLoadEntities: true,
-    synchronize: true
-  }),UsersModule, TodoModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    TodoModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
